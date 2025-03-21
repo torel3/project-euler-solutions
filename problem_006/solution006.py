8339bdcfe79b92365328aac27bf2d258d395240e
torel3 <120301682+torel3@users.noreply.github.com>
2025-03-21 14:59:23 +0100

    Add files via upload

--- problem_006/solution006.py
+++ problem_006/solution006.py
@@ -8,11 +8,17 @@ def sum_of_squares(n):
         res += i*i
     return res
 
+def sum_of_squares_optimized(n):
+    return n*(n+1)*(2*n+2)/6
+
 def square_of_sum(n):
     return (n*(n+1)/2)**2
 
 def sum_square_difference(n):
     return int(square_of_sum(n) - sum_of_squares(n))
 
+def sum_square_difference_optimized(n):
+    return int(square_of_sum(n) - sum_of_squares_optimized(n))
+
 n = 100
-print("The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is:", sum_square_difference(n))
+print("The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is:", sum_square_difference_optimized(n))
