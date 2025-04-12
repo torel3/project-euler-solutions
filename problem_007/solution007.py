1677ebffb08daa6a8100039aeac1292862b21dbe
torel3 <120301682+torel3@users.noreply.github.com>
2025-04-12 20:41:44 +0200

    Add files via upload

---
+++ problem_007/solution007.py
@@ -0,0 +1,21 @@
+# Solution to Project Euler Problem 7
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+def nth_prime(n):
+    count = 0
+    num = 1
+    while count < n:
+        num += 1
+        if is_prime(num):
+            count += 1
+    return num
+
+# Find the 10,001st prime
+print(nth_prime(10001))
